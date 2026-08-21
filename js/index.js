2d98271cb7f036ad1296fedabcc75a618a2b8660
yumerry1203 <moa3627@naver.com>
2026-08-21 15:27:03 +0900

    feat : sub page 추가

--- js/index.js
+++ js/index.js
@@ -1,65 +1,87 @@
-$(document).ready(function(){
-    $(window).scroll(function () { 
-        const scrollValue = $(document).scrollTop(); 
+$(document).ready(function () {
+    $(window).scroll(function () {
+        const scrollValue = $(document).scrollTop();
         const section1Top = $('#section1').offset().top;
-        if(scrollValue >= section1Top){
-            $('.sticky').css({"opacity":1});
-        }else{
-            $('.sticky').css({"opacity":0});
+        if (scrollValue >= section1Top) {
+            $('.sticky').css({ "opacity": 1 });
+        } else {
+            $('.sticky').css({ "opacity": 0 });
         }
     });
-    
+
+    /* 구매하기  */
+    $(".ott_item li").click(function () {
+        $(this).siblings().removeClass('on');
+        $(this).addClass('on');
+        $(this).parent().parent().next().stop().fadeIn();
+    });
+    $(".netflix_price li, .youtube_price li").click(function () {
+        $(this).siblings().children().children().attr("src", "image/ticket_black.png");
+        $(this).siblings().removeClass('active');
+        $(this).addClass('active');
+        $('.netflix_price li.active, .youtube_price li.active').children().children().attr("src", "image/ticket_white.png");
+        $(this).parent().parent().parent().next().stop().fadeIn();
+
+        const offset = $('#total_price').offset();
+        $('html, body').animate({ scrollTop: offset.top }, 500);
+    });
+
+    /* 마이페이지 - 복사 완료 토스트 메세지 */
+    const toast = document.querySelector(".alert-copy");
+    const copyBtn = document.querySelectorAll('img.copy');
+    let isToastShown = false;
+    copyBtn.forEach(function (btn) {
+       
+        btn.addEventListener('click', function () {
+            if(isToastShown) return;
+            isToastShown = true;
+            toast.classList.add("show");
+            setTimeout(function () {
+                toast.classList.remove("show");
+                isToastShown = false;
+            }, 2700);
+        })
+    });
+
+    /* 자주 묻는 질문 = 아코디언 */
+    $('.question').click(function () {
+        $(this).siblings(".question").removeClass("active");
+        $(this).siblings(".answer").stop().slideUp();
+        $(this).next().stop().slideToggle();
+    });
+
 
 });
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     var modal = document.getElementById("myModal");
     var btn = document.getElementById("loginBtn");
     var closeBtn = document.getElementsByClassName("close")[0];
     var kakaoLoginBtn = document.getElementById("kakaoLoginBtn");
 
-    btn.onclick = function() {
+    btn.onclick = function () {
         modal.style.display = "flex";
     }
 
-    closeBtn.onclick = function() {
+    closeBtn.onclick = function () {
         modal.style.display = "none";
     }
 
-    window.onclick = function(event) {
+    window.onclick = function (event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
 
-    kakaoLoginBtn.onclick = function() {
+    kakaoLoginBtn.onclick = function () {
         window.location.href = "/login";
     }
-    });
-
-    
-/*     window.addEventListener('scroll', function() {
-    var stickyButton = document.getElementById('sticky-button');
-    var mainSection = document.querySelector('main');
-    var footerSection = document.querySelector('footer');
-
-    var mainBottom = mainSection.getBoundingClientRect().bottom;
-    var footerTop = footerSection.getBoundingClientRect().top;
-    var windowHeight = window.innerHeight;
-
-    const section2Top = this.document.querySelector('#section2');
-
-    if (mainBottom <= windowHeight && footerTop > windowHeight) {
-        stickyButton.style.opacity = '1';
-    } else {
-        stickyButton.style.opacity = '0';
-    }
-}); */
+});
 
 
 /* GSAP 효과 */
-    let funcObj={
-        f_0:function(){
+let funcObj = {
+    f_0: function () {
         const tl = gsap.timeline();
         tl.to("#section0", {
             opacity: 1,
@@ -70,33 +92,33 @@ document.addEventListener('DOMContentLoaded', function() {
             opacity: 1,
             stagger: 0.3,
         });
-        },
-        f_1:function(){
+    },
+    f_1: function () {
         const tl = gsap.timeline();
         tl.to("#section1 > *", {
             opacity: 1,
             stagger: 0.3,
             y: -30,
         });
-        },
-        f_2:function(){
+    },
+    f_2: function () {
         const tl = gsap.timeline();
         tl.to("#section2 > *", {
             opacity: 1,
             stagger: 0.3,
             y: -30,
         });
-        },
-        f_3:function(){
+    },
+    f_3: function () {
         const tl = gsap.timeline();
         tl.to("#section3 > *", {
             opacity: 1,
             stagger: 0.3,
         });
-        },
-        f_4:function(){
+    },
+    f_4: function () {
         const tl = gsap.timeline();
-    
+
         tl.to("#section4 > *", {
             opacity: 1,
             stagger: 0.3,
@@ -107,23 +129,23 @@ document.addEventListener('DOMContentLoaded', function() {
             stagger: 0.3,
             y: -30,
         });
-        },
-    };
-    
-    let section = document.querySelectorAll('section');
-    
-    funcObj['f_0'](); 
-    
-    window.addEventListener('scroll', function(){
-    
-        let scroll = document.scrollingElement.scrollTop;
-        let outHeight = this.window.outerHeight; 
+    },
+};
 
-        for(let i =0; i<section.length;i++){
-        if(scroll > section[i].offsetTop - outHeight + 200&&
-            scroll < section[i].offsetTop - outHeight + section[i].offsetHeight){
-            funcObj['f_'+i]();
+let section = document.querySelectorAll('section');
+
+funcObj['f_0']();
+
+window.addEventListener('scroll', function () {
+
+    let scroll = document.scrollingElement.scrollTop;
+    let outHeight = this.window.outerHeight;
+
+    for (let i = 0; i < section.length; i++) {
+        if (scroll > section[i].offsetTop - outHeight + 250 &&
+            scroll < section[i].offsetTop - outHeight + section[i].offsetHeight) {
+            funcObj['f_' + i]();
         }
-        };
-    
-    });
+    };
+
+});
